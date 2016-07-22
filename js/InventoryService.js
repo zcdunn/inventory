3cece741ffa128cf593eb94bf353e0116ebf2370
Zachary Dunn <zdunn76@gmail.com>
2016-07-22 17:34:27 -0400

    Replace icons with transparent icons Add type parameter to inventories Update index

--- js/InventoryService.js
+++ js/InventoryService.js
@@ -51,36 +51,39 @@ myApp.service('inventoryService', function($http) {
 
     this.defaultJson = {
         "76ee1ec6-b9db-a9c6-48ae-4f452273cfb8": {
-            "id": "76ee1ec6-b9db-a9c6-48ae-4f452273cfb8",
-            "name": "Lionshead Coster",
-            "coin": {
+            id: "76ee1ec6-b9db-a9c6-48ae-4f452273cfb8",
+            name: "Lionshead Coster",
+            type: "shop",
+            coin: {
                 "gp": 500,
                 "sp": 0,
                 "cp": 0
             },
-            "items": {}
+            items: {}
         },
 
         "d03204e1-8f7c-4905-4d9f-de161c7b4f14": {
-            "id": "d03204e1-8f7c-4905-4d9f-de161c7b4f14",
-            "name": "Phandalin Smithy",
-            "coin": {
+            id: "d03204e1-8f7c-4905-4d9f-de161c7b4f14",
+            name: "Phandalin Smithy",
+            type: "shop",
+            coin: {
                 "gp": 250,
                 "sp": 0,
                 "cp": 0
             },
-            "items": {}
+            items: {}
         },
 
         "9ef00a86-f484-5a00-b000-56dbfbb37664": {
-            "id": "9ef00a86-f484-5a00-b000-56dbfbb37664",
-            "name": "Phandalin Miner's Exchange",
-            "coin": {
+            id: "9ef00a86-f484-5a00-b000-56dbfbb37664",
+            name: "Phandalin Miner's Exchange",
+            type: "shop",
+            coin: {
                 "gp": 450,
                 "sp": 325,
                 "cp": 750
             },
-            "items": {}
+            items: {}
         }
     };
 });
