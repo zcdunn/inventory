23e3d4351b5c21ebcd9b588db8d3f596d8834625
Zachary Dunn <zdunn76@gmail.com>
2016-07-22 20:16:44 -0400

    Add mutationObserver to upgrade material components on view change

--- js/app.js
+++ js/app.js
@@ -1,33 +1,51 @@
-var myApp = angular.module('inventory', [ 'ngRoute' ]);
-myApp.config(function($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/allInventories.html',
-            controller: 'InventoryController'
-        })
-        .when('/edit/:id', {
-            templateUrl: 'views/editInventory.html',
-            controller: 'InventoryController'
-        })
-        .when('/edit/:id/item/new/:itemId', {
-            templateUrl: 'views/editItem.html',
-            controller: 'InventoryController'
-        })
-        .when('/new/:id', {
-            templateUrl: 'views/editInventory.html',
-            controller: 'InventoryController'
-        })
-        .when('/view/:id', {
-            templateUrl: 'views/viewInventory.html',
-            controller: 'InventoryController'
-        })
-        .when('/view/:id/item/:itemId', {
-            templateUrl: 'views/viewItem.html',
-            controller: 'InventoryController'
-        })
-        .otherwise({
-            redirectTo: '/'
+var myApp = angular
+    .module('inventory', [ 'ngRoute' ])
+    .config(function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/allInventories.html',
+                controller: 'InventoryController'
+            })
+            .when('/edit/:id', {
+                templateUrl: 'views/editInventory.html',
+                controller: 'InventoryController'
+            })
+            .when('/edit/:id/item/new/:itemId', {
+                templateUrl: 'views/editItem.html',
+                controller: 'InventoryController'
+            })
+            .when('/new/:id', {
+                templateUrl: 'views/editInventory.html',
+                controller: 'InventoryController'
+            })
+            .when('/view/:id', {
+                templateUrl: 'views/viewInventory.html',
+                controller: 'InventoryController'
+            })
+            .when('/view/:id/item/:itemId', {
+                templateUrl: 'views/viewItem.html',
+                controller: 'InventoryController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+        // user HTML5 History API
+        $locationProvider.html5Mode(true);
+    })
+    .run(function() {
+        var mdlUpgradeDom = false;
+        setInterval(function() {
+            if(mdlUpgradeDom) {
+                componentHandler.upgradeDom();
+                mdlUpgradeDom = false;
+            }
+        }, 200);
+
+        var observer = new MutationObserver(function() {
+            mdlUpgradeDom = true;
         });
-    // user HTML5 History API
-    $locationProvider.html5Mode(true);
-});
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
