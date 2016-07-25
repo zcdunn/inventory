b0bbde245fd4f74e420e36293d8392c05f3ec132
Zachary Dunn <zdunn76@gmail.com>
2016-07-25 14:37:55 -0400

    Add location path to console

--- js/InventoryController.js
+++ js/InventoryController.js
@@ -2,6 +2,7 @@ myApp.controller('InventoryController', function($scope, $window, $location, $ro
     $scope.inventories = inventoryService.loadInventories();
     $scope.inventory = inventoryService.getInventory($routeParams.id);
     $scope.headerIcon = $scope.inventory ? "arrow_back" : "";
+    console.log("Path: " + $location.path());
 
     $scope.goBack = function() {
         $window.history.go(-1);
