1bb9358f52e36d44cc71851dcb4b4478fcda2c11
Zachary Dunn <zdunn76@gmail.com>
2016-08-05 14:31:04 -0400

    Add dialog element to viewInventory Update mdl-jquery-modal-dialog.js to use existing dialog instead of creating one

--- js/mdl-jquery-modal-dialog.js
+++ js/mdl-jquery-modal-dialog.js
@@ -17,91 +17,76 @@ function hideLoading() {
 }
 
 function showDialog(options) {
-    options = $.extend({
-        id: 'orrsDiag',
-        title: null,
-        text: null,
+    opts = $.extend({
+        selector: '.dialog-container',
         negative: false,
         positive: false,
         cancelable: true,
-        contentStyle: null,
         onLoaded: false
     }, options);
 
-    // remove existing dialogs
-    $('.dialog-container').remove();
     $(document).unbind("keyup.dialog");
-
-    $('<div id="' + options.id + '" class="dialog-container"><div class="mdl-card mdl-shadow--16dp"></div></div>').appendTo("body");
-    var dialog = $('#orrsDiag');
+    var dialog = $(opts.selector).remove();
     var content = dialog.find('.mdl-card');
-    if (options.contentStyle != null) content.css(options.contentStyle);
-    if (options.title != null) {
-        $('<h5>' + options.title + '</h5>').appendTo(content);
-    }
-    if (options.text != null) {
-        $('<p>' + options.text + '</p>').appendTo(content);
-    }
-    if (options.negative || options.positive) {
-        var buttonBar = $('<div class="mdl-card__actions dialog-button-bar"></div>');
-        if (options.negative) {
-            options.negative = $.extend({
-                id: 'negative',
+    var buttonBar = dialog.find('.dialog-button-bar');
+
+    if(buttonBar.length !== 0) {
+        if(opts.negative) {
+            opt.negative = $.extend({
+                selector: '#negative',
                 title: 'Cancel',
-                onClick: function () {
+                onClick: function() {
                     return false;
                 }
-            }, options.negative);
-            var negButton = $('<button class="mdl-button mdl-js-button mdl-js-ripple-effect" id="' + options.negative.id + '">' + options.negative.title + '</button>');
-            negButton.click(function (e) {
+            }, opts.negative);
+            var negButton = buttonBar.find(opts.negative.selector);
+            negButton.click(function(e) {
                 e.preventDefault();
-                if (!options.negative.onClick(e))
-                    hideDialog(dialog)
+                if(!opts.negative.onClick(e))
+                    hideDialog(dialog);
             });
-            negButton.appendTo(buttonBar);
         }
-        if (options.positive) {
-            options.positive = $.extend({
-                id: 'positive',
+        if(opts.positive) {
+            opts.positive = $.extend({
+                selector: '#positive',
                 title: 'OK',
-                onClick: function () {
+                onClick: function() {
                     return false;
                 }
-            }, options.positive);
-            var posButton = $('<button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" id="' + options.positive.id + '">' + options.positive.title + '</button>');
-            posButton.click(function (e) {
+            }, opts.positive);
+            var posButton = buttonBar.find(opts.positive.selector);
+            posButton.click(function(e) {
                 e.preventDefault();
-                if (!options.positive.onClick(e))
-                    hideDialog(dialog)
+                if(!opts.positive.onClick(e))
+                    hideDialog(dialog);
             });
-            posButton.appendTo(buttonBar);
         }
-        buttonBar.appendTo(content);
     }
+    $('body').append(dialog);
     componentHandler.upgradeDom();
-    if (options.cancelable) {
-        dialog.click(function () {
+    if(opts.cancelable) {
+        dialog.click(function() {
             hideDialog(dialog);
         });
-        $(document).bind("keyup.dialog", function (e) {
-            if (e.which == 27)
+        $(document).bind("keyup.dialog", function(e) {
+            if(e.which == 27)
                 hideDialog(dialog);
         });
-        content.click(function (e) {
+        content.click(function(e) {
             e.stopPropagation();
         });
     }
-    setTimeout(function () {
+    setTimeout(function() {
         dialog.css({opacity: 1});
-        if (options.onLoaded)
-            options.onLoaded();
+        if(opts.onLoaded)
+            opts.onLoaded();
     }, 1);
 }
 
 function hideDialog(dialog) {
     $(document).unbind("keyup.dialog");
     dialog.css({opacity: 0});
-    setTimeout(function () {
+    setTimeout(function() {
         dialog.remove();
     }, 400);
 }
