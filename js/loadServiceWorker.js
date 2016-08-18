88cc8adb2deb18e086a2d444e4d2d321177fe85c
Zachary Dunn <zdunn76@gmail.com>
2016-08-18 11:37:19 -0400

    Show toast when app is offline ready

--- js/loadServiceWorker.js
+++ js/loadServiceWorker.js
@@ -1,5 +1,22 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('worker.js')
-        .then(reg => console.log('Registration succeeded. Scope is ' + reg.scope))
+        .then(reg => {
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+            // Let the user know once the app is available offline
+            var newWorker = reg.installing;
+            newWorker.onstatechange = event => {
+                if(newWorker.state == 'activated' && !navigator.serviceWorker.controller) {
+                    var notification = document.querySelector('.mdl-js-snackbar');
+                    notification.MaterialSnackbar.showSnackbar({
+                        message: 'Ready to work offline',
+                        actionText: 'Dismiss',
+                        actionHandler: function() {
+                            notification.classList.remove("mdl-snackbar--active");
+                        },
+                        timeout: 5000
+                    });
+                }
+            };
+        })
         .catch(error => console.log('Registration failed with ' + error));
 }
